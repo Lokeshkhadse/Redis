70fd07388bf91377096c4a9d958c18a7fca5901a
Lokesh Khadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-06-02 18:59:36 +0530

    Add files via upload

---
+++ Redis/src/main/java/com/example/Redis/redisConfig/redisConfig.java
@@ -0,0 +1,38 @@
+package com.example.Redis.redisConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class redisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+
+        return new LettuceConnectionFactory();
+    }
+
+
+    @Bean
+    public RedisTemplate<String,Object>redisTemplate(){
+
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+
+        //1.connectionfactory
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        //2.key serializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        //3.value serializer
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+
+        return redisTemplate;
+    }
+}
